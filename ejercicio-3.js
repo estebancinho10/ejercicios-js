58418aae98d823360f8be13891b348b8d1712e6c
estebancinho <130401845+estebancinho10@users.noreply.github.com>
2023-04-17 17:50:03 -0500

    ejercicio 3

---
+++ ejercicio-3.js
@@ -0,0 +1,10 @@
+const pi = 3.1416
+
+console.log(`el valor de pi es: ${pi}`)
+console.log(`el valor de pi es: ${pi}`)
+ 
+const segundos_en_minuto = 60
+const minutos_en_horas = 60
+const horas_en_dia = 24
+
+console.log(`un minuto tiene ${segundos_en_minuto} segundos una hora tiene ${minutos_en_horas} minutos y un dia tiene ${horas_en_dia} horas`)

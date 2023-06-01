b79e1a5d1cf4a6a0a4c84786aef1d075583de803
estebancinho <130401845+estebancinho10@users.noreply.github.com>
2023-06-01 17:06:39 -0500

    calculadora

---
+++ calculadora.ts
@@ -0,0 +1,35 @@
+// variable
+
+let num1: number = 6
+let num2: number = 6
+
+//constantes
+
+console.log(num1, num2)
+
+// operaciones
+
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(sumar(num1, num2))
+
+
+function restar(a: number, b: number): number {
+    return a - b;
+}
+
+console.log(restar(num1, num2))
+
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiplicar(num1, num2))
+
+function dividir(a: number, b: number): number {
+    return a / b;
+}
+
+console.log(dividir(num1, num2))
